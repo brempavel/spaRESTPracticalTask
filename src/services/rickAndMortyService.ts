90d1f6954532fd563ffde60a3dca70f9cfdb6ea4
Pavlo Brem <pavlo.brem@valtech.com>
2023-10-13 16:44:36 +0300

    add error handling

--- src/services/rickAndMortyService.ts
+++ src/services/rickAndMortyService.ts
@@ -1,10 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const baseUrl = 'https://rickandmortyapi.com/api/character';
 
 export const getCharacters = async (page: number = 1, name?: string) => {
-	const { data } = await axios.get(baseUrl, {
-		params: { page, name },
-	});
-	return data;
+	let response;
+	try {
+		response = await axios.get(baseUrl, {
+			params: { page, name },
+		});
+	} catch (e: unknown) {
+		response = (e as AxiosError).response;
+	}
+	return response?.data;
 };
