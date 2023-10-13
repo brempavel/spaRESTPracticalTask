052bf3cc97d868559529f39e557b75ce975a0dd1
Pavlo Brem <pavlo.brem@valtech.com>
2023-10-13 16:35:27 +0300

    add font family

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import store from './store.ts';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './style/chakra.ts';
 
 const queryClient = new QueryClient();
 
@@ -16,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		<Router>
 			<Provider store={store}>
 				<QueryClientProvider client={queryClient}>
-					<ChakraProvider>
+					<ChakraProvider theme={theme}>
 						<App />
 					</ChakraProvider>
 				</QueryClientProvider>
