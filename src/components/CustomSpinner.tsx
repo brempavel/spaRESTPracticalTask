507b3819c47fc578a1d337c3f06a5eda60974472
Pavlo Brem <pavlo.brem@valtech.com>
2023-10-13 16:42:21 +0300

    fix spinner styles

--- src/components/CustomSpinner.tsx
+++ src/components/CustomSpinner.tsx
@@ -1,15 +1,21 @@
-import { Spinner } from '@chakra-ui/react';
+import { Modal, ModalOverlay, Spinner } from '@chakra-ui/react';
+
+const widthInPx = 100;
 
 const CustomSpinner = () => {
 	return (
-		<Spinner
-			w="100px"
-			h="100px"
-			color="black"
-			pos="absolute"
-			left="50vw"
-			bottom="50vh"
-		/>
+		<Modal isOpen onClose={() => {}}>
+			<ModalOverlay bg="none" backdropFilter="auto" backdropBlur="2px">
+				<Spinner
+					w={`${widthInPx}px`}
+					h={`${widthInPx}px`}
+					color="black"
+					pos="absolute"
+					left={`calc(50vw - ${widthInPx / 2}px)`}
+					bottom="50vh"
+				/>
+			</ModalOverlay>
+		</Modal>
 	);
 };
 
